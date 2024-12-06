f4224bee0fc5fe0db9b3b8c4103285f474a3faf6
bukan-tapio <pio.tapio90@gmail.com>
2024-12-06 10:31:34 +0700

    update: for port

--- api/testPath.js
+++ api/testPath.js
@@ -1,8 +1,13 @@
 import express from "express";
 const app = express();
+const port = parseInt(process.env.PORT) || 8080;
 
 app.get("/", (req, res) => {
   res.send({
     message: "sudah ready",
   });
+});
+
+app.listen(port, () => {
+  console.log(`use this port: ${port}`);
 });
